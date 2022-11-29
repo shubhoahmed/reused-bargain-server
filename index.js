9f7058dbb48465d714d7cdfaeb69a9729a95dab8
shubhoahmed <shubhoahmed03@gmail.com>
2022-11-29 06:36:50 +0600

    users

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-//midleware...
+//This is for middleware 
 app.use(cors());
 app.use(express.json());
 
@@ -17,7 +17,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-        const userCollection = client.db('reused-bargain').collection('user');
+        const usersCollection = client.db('reused-bargain').collection('user');
         const categoryCollection = client.db('reused-bargain').collection('cars-category');
         const productCollection = client.db('reused-bargain').collection('products');
 
@@ -34,13 +34,32 @@ async function run() {
             console.log(result);
             res.send(result);
         });
-        app.get('/user', async (req, res) => {
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            console.log(email);
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '2h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        });
+
+        app.get('/users', async (req, res) => {
             const query = {};
-            const options = await userCollection.find(query).toArray();
+            const options = await usersCollection.find(query).toArray();
             res.send(options);
         });
-        app.post("/user", async (req, res) => {
-            const result = await userCollection.insertOne(req.body)
+        app.post("/users", async (req, res) => {
+            const user = usersCollection.findOne({ email: req.body.email });
+
+            if (user) {
+                return res.send(user);
+            }
+            const result = await usersCollection.insertOne(req.body)
+            res.send(result)
         });
     }
     finally {
